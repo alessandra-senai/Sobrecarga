6a49ca7a00eddfcb2a01f415c89edfce433a1c8e
Alessandra Soares dos Santos <AlessandraSoaresdosSantos@users.noreply.github.com>
2022-03-03 21:33:01 -0300

    inclusão saída no console

--- SobrecargaConsole/Program.cs
+++ SobrecargaConsole/Program.cs
@@ -8,8 +8,38 @@ namespace SobrecargaConsole
         static void Main(string[] args)
         {
 
+            int resultadoSomar = Calculadora.Somar(5, 6);
 
-         
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine($"Somar números inteiros = {resultadoSomar}");
+
+            var resultadoSomar2 = Calculadora.Somar(54, 65.0);
+
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine($"Somar números tipo double = {resultadoSomar2}");
+
+            int resultadoMaiorValor = Calculadora.MaiorValor(100, 250);
+
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine($"maior valor - tipo inteiro = {resultadoMaiorValor}");
+
+            decimal resultadoMenorValor = Calculadora.MenorValor(45, 48);
+
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine($"menor valor - tipo inteiro = {resultadoMenorValor}");
+
+
+            Produto produto = new Produto(45, "celular Samsung", "informática", 4567.78M, 54);
+
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine($"produto - código {produto.Codigo}");
+            Console.WriteLine($"produto - nome {produto.Nome}");
+            Console.WriteLine($"produto - categoria {produto.Categoria}");
+            Console.WriteLine($"produto - preço {produto.Preco}");
+            Console.WriteLine($"produto - quantidade {produto.Quantidade}");
+
+            Console.WriteLine("---------------------------------------------");
+
         }
     }
 }
